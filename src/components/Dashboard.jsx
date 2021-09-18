8ddc248ff6e29d6245391fd13635b278b8247a20
David Montoya Ramírez <deividelpaisa@hotmail.com>
2021-09-18 02:28:14 -0500

    first commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import  {Apiurl}  from "../services/apirest";
 import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap'
 
 class Dashboard extends React.Component {
 
     state= {
-        carros: []
+        carros: [],
+        modalInsertar: false,
+        form: {
+            id: "",
+            conductor: "",
+            entrada: "",
+            salida: "",
+            estado: "",
+        }
     }
 
-    clickCarro(id){
-        let url = Apiurl + "IS/V1/carros/" + id;
-        axios.put(url , {
-            estado:true
-        })
+    modalInsertar=() => {
+        this.setState({modalInsertar: !this.state.modalInsertar});
+    }
+
+    handleChange= async e => {
+        e.persist();
+        await this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        });
+        console.log(this.state.form);
+    }
+
+    peticionPost=async()=>{
+        let url = Apiurl + "IS/V1/carros";
+       await axios.post(url, this.state.form)
         .then(response => {
-            this.setState({
-                /* carros : response.data.mensaje.estado = true  */
-            })
-            console.log(response);
+            this.modalInsertar();
+            this.componentDidMount();
+        }).catch(error =>{
+            console.log(error.message);
         })
     }
 
@@ -28,13 +50,30 @@ class Dashboard extends React.Component {
             this.setState({
                 carros : response.data.mensaje
             })
+        })
+    }
+
+    clickCarro(id){
+        let url = Apiurl + "IS/V1/carros/" + id;
+        axios.put(url , {
+               estado: false
+        })
+        .then(response => {
+            this.setState({
+                estado: false
+            })
             console.log(response);
         })
     }
 
     render() {
+        const {form} = this.state;
         return(
-            <table className="table w-50 mt-5 ms-5">
+
+            <>
+            <div className="d-flex flex-column align-items-center container-fluid"> 
+            <button className="btn btn-success mt-5 w-50" onClick={() => this.modalInsertar()}>Ingresar Vehiculo</button>
+            <table className="table w-100 mt-5 ms-5">
   <thead>
     <tr>
       <th scope="col">Placa</th>
@@ -55,7 +94,7 @@ class Dashboard extends React.Component {
                 <td>{value.entrada}</td>
                 <td>{value.salida}</td>
                 <td>{value.estado ? "parqueado" : "salio"}</td>
-                <td>{value.pago}</td>
+                <td>{new Intl.NumberFormat("en-EN").format(value.pago)}</td>
                 <td>
                     <button className="btn btn-danger me-4" onClick={() => this.clickCarro(value.id)}>Sacar carro</button>
                 </td>
@@ -65,6 +104,42 @@ class Dashboard extends React.Component {
     
   </tbody>
 </table>
+
+<Modal isOpen={this.state.modalInsertar}>
+    <ModalHeader style={{display: "block"}}>
+        <span style={{float: "right"}}>x</span>
+    </ModalHeader>
+    <ModalBody>
+        <div className="form-group">
+            <label html for="id">Placa</label>
+            <input className= "form-control" type="text" name="id" onChange={this.handleChange} value={form.id}/>
+            <br/>
+            <label html for="conductor">Conductor</label>
+            <input className= "form-control" type="text" name="conductor" onChange={this.handleChange} value={form.conductor}/>
+            <br/>
+            <label html for="entrada">Entrada</label>
+            <input className= "form-control" type="text" name="entrada" readOnly onChange={this.handleChange} value={form.entrada}/>
+            <br/>
+            <label html for="salida">Salida</label>
+            <input className= "form-control" type="text" name="salida" readOnly onChange={this.handleChange} value={form.salida}/>
+            <br/>
+            <label html for="estado">Estado</label>
+            <input className= "form-control" type="text" name="estado" readOnly onChange={this.handleChange} value={form.estado}/>
+            <br/>
+        </div>
+    </ModalBody>
+
+    <ModalFooter>
+        <button className= "btn btn-success" onClick={() => this.peticionPost()}>
+            Insertar
+        </button>
+        <button className= "btn btn-danger" onClick={() => this.modalInsertar()}>
+            Cancelar
+        </button>
+    </ModalFooter>
+</Modal>
+</div>
+</>
         )
     }
 }
